f6081fd9947a1b2fc3f999bb22cc954dcc5e1bbc
GAMIS65 <ivicko05@gmail.com>
2020-05-18 12:38:30 +0200

    Sort of fixed #2

--- FileSorter.py
+++ FileSorter.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import tkinter  # python -m pip install tkinter
+import uuid
 from tkinter import filedialog
 
 root = tkinter.Tk()
@@ -13,6 +14,7 @@ s = os.chdir(user_directory)
 current = os.getcwd()
 
 files = os.listdir(current)
+
 
 # File extensions
 images = [".png", ".jpg", ".jpeg", ".gif", ".jfif", ".webp"]  # File extensions for images
@@ -50,54 +52,61 @@ if not os.path.exists('Archives_Sorted'):
 # Sort files in the folders
 for file in files:
     destination = ""
-    for ex in images:
-        if file.endswith(ex):
-            destination = f'{user_directory}\Images_Sorted'
-            shutil.move(file, destination)
-            print(f"Moved {file} to {user_directory}\Images_Sorted")
-            break
+    try:
+        for ex in images:
+            if file.endswith(ex):
+                destination = f'{user_directory}\Images_Sorted'
+                shutil.move(file, destination)
+                print(f"Moved {file} to {user_directory}\Images_Sorted")
+                break
 
-    for ex in videos:
-        if file.endswith(ex):
-            destination = f'{user_directory}\Videos_Sorted'
-            shutil.move(file, destination)
-            print(f"Moved {file} to {user_directory}\Videos_Sorted")
-            break
+        for ex in videos:
+            if file.endswith(ex):
+                destination = f'{user_directory}\Videos_Sorted'
+                shutil.move(file, destination)
+                print(f"Moved {file} to {user_directory}\Videos_Sorted")
+                break
 
-    for ex in sounds:
-        if file.endswith(ex):
-            destination = f'{user_directory}\Sounds_Sorted'
-            shutil.move(file, destination)
-            print(f"Moved {file} to {user_directory}\Sounds_Sorted")
-            break
+        for ex in sounds:
+            if file.endswith(ex):
+                destination = f'{user_directory}\Sounds_Sorted'
+                shutil.move(file, destination)
+                print(f"Moved {file} to {user_directory}\Sounds_Sorted")
+                break
 
-    for ex in text:
-        if file.endswith(ex):
-            destination = f'{user_directory}\Texts_Sorted'
-            shutil.move(file, destination)
-            print(f"Moved {file} to {user_directory}\Texts_Sorted")
-            break
+        for ex in text:
+            if file.endswith(ex):
+                destination = f'{user_directory}\Texts_Sorted'
+                shutil.move(file, destination)
+                print(f"Moved {file} to {user_directory}\Texts_Sorted")
+                break
 
-    for ex in applications:
-        if file.endswith(ex):
-            destination = f'{user_directory}\Applications_Sorted'
-            shutil.move(file, destination)
-            print(f"Moved {file} to {user_directory}\Applications_Sorted")
-            break
+        for ex in applications:
+            if file.endswith(ex):
+                destination = f'{user_directory}\Applications_Sorted'
+                shutil.move(file, destination)
+                print(f"Moved {file} to {user_directory}\Applications_Sorted")
+                break
 
-    for ex in code:
-        if file.endswith(ex):
-            destination = f'{user_directory}\Code_Sorted'
-            shutil.move(file, destination)
-            print(f"Moved {file} to {user_directory}\Code_Sorted")
-            break
+        for ex in code:
+            if file.endswith(ex):
+                destination = f'{user_directory}\Code_Sorted'
+                shutil.move(file, destination)
+                print(f"Moved {file} to {user_directory}\Code_Sorted")
+                break
 
-    for ex in archives:
-        if file.endswith(ex):
-            destination = f'{user_directory}\Archives_Sorted'
-            shutil.move(file, destination)
-            print(f"Moved {file} to {user_directory}\Archives_Sorted")
-            break
+        for ex in archives:
+            if file.endswith(ex):
+                destination = f'{user_directory}\Archives_Sorted'
+                shutil.move(file, destination)
+                print(f"Moved {file} to {user_directory}\Archives_Sorted")
+                break
 
+    except Exception as err:    # When a file with the same name exists it will rename the file
+        extension = os.path.splitext(file)[1]   # Get the file extension
+        xx = str(uuid.uuid4()) + extension  #
+        os.rename(file, file[1] + xx)
+        print("FILE CONFLICT || Please restart the program to solve this issue")    # TODO FIX THIS
+        continue    # This continue should work but it doesn't. That's why the print above exists.
 
 input("[+] Files sorted you can close this window now")
