17da317edf009108357786c54894b233e871be12
GAMIS65 <ivicko05@gmail.com>
2020-08-15 22:38:52 +0200

    Added functions

--- FileSorter.py
+++ FileSorter.py
@@ -5,108 +5,77 @@ import uuid
 from tkinter import filedialog
 
 root = tkinter.Tk()
-root.withdraw()
+root.withdraw()     # This hides the blank tkinter window
 
+def get_directory():
+    global user_directory 
+    global files
+    
+    print("Select a directory you want to sort")
+    
+    user_directory = tkinter.filedialog.askdirectory(initialdir="/",  title='Please select a directory')
+    s = os.chdir(user_directory)
+    current = os.getcwd()
+    files = os.listdir(current)
 
-print("Select a directory you want to sort")
-user_directory = tkinter.filedialog.askdirectory(initialdir="/",  title='Please select a directory')
-s = os.chdir(user_directory)
-current = os.getcwd()
-
-files = os.listdir(current)
+    return user_directory, files
 
 
 # File extensions
 images = [".png", ".jpg", ".jpeg", ".gif", ".jfif", ".webp"]  # File extensions for images
 videos = [".mp4", ".mov", ".webm", ".mkv", ".flv", ".avi"]  # File extensions for videos
 sounds = [".mp3", ".wav", ".flac", ".ogg"]  # File extensions for sounds
-text = [".txt", ".pdf", ".doc", ".docx", ".odt"]  # File extensions for text documents
+text = [".txt", ".pdf", ".doc", ".docx", ".odt", ".ppt", ".pptx", ".pptm"]  # File extensions for text documents
 applications = [".exe", ".jar"]  # File extension for applications
 code = [".py", ".cpp", ".js", ".java", ".cs", ".php", ".c", ".html", ".css", ".lua"]    # File extensions for code
 archives = [".rar", ".zip", ".7z", ".tar", ".gz"]  # File extensions for archives
 
-print("Sorting your files")
 
-# Make folders if they don't exist
-if not os.path.exists('Images_Sorted'):
-    os.makedirs('Images_Sorted')
+def make_folders(folder_name):
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
 
-if not os.path.exists('Videos_Sorted'):
-    os.makedirs('Videos_Sorted')
 
-if not os.path.exists('Sounds_Sorted'):
-    os.makedirs('Sounds_Sorted')
+def sort_files(files, file_formats, folder_name, user_directory): 
+    try:    
+        for file in files:
+            destination = ""
+            for file_format in file_formats:
+                if file.endswith(file_format):
+                    destination = f'{user_directory}\\{folder_name}'
+                    shutil.move(file, destination)
+                    print(f"Moved {file} to {user_directory}/{folder_name}")
+                    break
 
-if not os.path.exists('Texts_Sorted'):
-    os.makedirs('Texts_Sorted')
-
-if not os.path.exists('Applications_Sorted'):
-    os.makedirs('Applications_Sorted')
-
-if not os.path.exists('Code_Sorted'):
-    os.makedirs('Code_Sorted')
-
-if not os.path.exists('Archives_Sorted'):
-    os.makedirs('Archives_Sorted')
-
-# Sort files in the folders
-for file in files:
-    destination = ""
-    try:
-        for ex in images:
-            if file.endswith(ex):
-                destination = f'{user_directory}\Images_Sorted'
-                shutil.move(file, destination)
-                print(f"Moved {file} to {user_directory}\Images_Sorted")
-                break
-
-        for ex in videos:
-            if file.endswith(ex):
-                destination = f'{user_directory}\Videos_Sorted'
-                shutil.move(file, destination)
-                print(f"Moved {file} to {user_directory}\Videos_Sorted")
-                break
-
-        for ex in sounds:
-            if file.endswith(ex):
-                destination = f'{user_directory}\Sounds_Sorted'
-                shutil.move(file, destination)
-                print(f"Moved {file} to {user_directory}\Sounds_Sorted")
-                break
-
-        for ex in text:
-            if file.endswith(ex):
-                destination = f'{user_directory}\Texts_Sorted'
-                shutil.move(file, destination)
-                print(f"Moved {file} to {user_directory}\Texts_Sorted")
-                break
-
-        for ex in applications:
-            if file.endswith(ex):
-                destination = f'{user_directory}\Applications_Sorted'
-                shutil.move(file, destination)
-                print(f"Moved {file} to {user_directory}\Applications_Sorted")
-                break
-
-        for ex in code:
-            if file.endswith(ex):
-                destination = f'{user_directory}\Code_Sorted'
-                shutil.move(file, destination)
-                print(f"Moved {file} to {user_directory}\Code_Sorted")
-                break
-
-        for ex in archives:
-            if file.endswith(ex):
-                destination = f'{user_directory}\Archives_Sorted'
-                shutil.move(file, destination)
-                print(f"Moved {file} to {user_directory}\Archives_Sorted")
-                break
-
-    except Exception as err:    # When a file with the same name exists it will rename the file
-        extension = os.path.splitext(file)[1]   # Get the file extension
-        xx = str(uuid.uuid4()) + extension  #
+    except Exception as err:   # Rename the file if a file with that name already exists
+        extension = os.path.splitext(file)[1]   
+        xx = str(uuid.uuid4()) + extension  
         os.rename(file, file[1] + xx)
-        print("FILE CONFLICT || Please restart the program to solve this issue")    # TODO FIX THIS
-        continue    # This continue should work but it doesn't. That's why the print above exists.
+        s = os.chdir(user_directory)
+        current = os.getcwd()
+        files = os.listdir(current)
+        sort_files(files, file_formats, folder_name, user_directory)
 
-input("[+] Files sorted you can close this window now")
+def main():
+    get_directory()
+    
+    make_folders("Images_Sorted")
+    make_folders("Videos_Sorted")
+    make_folders("Sounds_Sorted")
+    make_folders("Archives_Sorted")
+    make_folders("Texts_Sorted")
+    make_folders("Code_Sorted")
+    make_folders("Applications_Sorted")
+
+    sort_files(files, images, "Images_Sorted", user_directory)
+    sort_files(files, videos, "Videos_Sorted", user_directory)
+    sort_files(files, sounds, "Sounds_Sorted", user_directory)
+    sort_files(files, archives, "Archives_Sorted", user_directory)
+    sort_files(files, text, "Texts_Sorted", user_directory)
+    sort_files(files, code, "Code_Sorted", user_directory)
+    sort_files(files, applications, "Applications_Sorted", user_directory)
+
+    input("Files sorted. Press any key to exit")
+
+if __name__ == "__main__":
+    main()
